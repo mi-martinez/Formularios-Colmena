f584959394af39ada81c14fc078090a364cab7ed
mi-martinez <info@mi-martinez.com>
2021-07-08 21:10:08 -0500

    PHP Fomulario base

---
+++ colmena.php
@@ -0,0 +1,185 @@
+<form action="" method="post">
+    <h1>Tu Diario Asegurado</h1>
+    <p>Solicitar tu seguro es fácil, por favor completa los siguientes datos.</p>
+    <div class="info-name">
+        <div class="material-input">
+            <input type="text" name="" id="" placeholder="&nbsp;">
+            <label>Primer Nombre:</label>
+        </div>
+        <div class="material-input">
+            <input type="text" name="" id="" placeholder="&nbsp;">
+            <label>Segundo Nombre:</label>
+        </div>
+    </div>
+    <div class="info-name">
+        <div class="material-input">
+            <input type="text" name="" id="" placeholder="&nbsp;">
+            <label>Primer Apellido:</label>
+        </div>
+        <div class="material-input">
+            <input type="text" name="" id="" placeholder="&nbsp;">
+            <label>Segundo Apellido:</label>
+        </div>
+    </div>
+    <div class="date-input material-input">
+        <input type="date" name="" id="">
+        <label>Fecha de nacimiento:</label>
+    </div>
+    <div class="info-name">
+        <div class="material-input">
+            <input type="email" name="" id="" placeholder="&nbsp;">
+            <label>Correo electrónico:</label>
+        </div>
+    </div>
+    <div class="info-name">
+        <div class="material-input">
+            <input type="tel" name="" id="" placeholder="&nbsp;">
+            <label>Número Celular:</label>
+        </div>
+    </div>
+    <div class="info-name">
+        <div class="material-inline">
+            <label class="contain-check">
+                <p class="aceppt">He leído y autorizo el <span class="acept"> tratamiento de datos personales.</span>
+                </p>
+                <input type="checkbox" name="" id="" data-required="1"><span class="checkmark" checked="checked"></span>
+                <span class="checkmark"></span>
+            </label>
+        </div>
+    </div>
+    <div class="row-submit"><input type="submit" value="Adquirir Seguro"></div>
+</form>
+
+<!-- SEGUNDO FORMULARIO -->
+
+<form action="" method="post">
+    <div class="steps">
+        <div class="step1 active">1</div>
+        <div class="step2">2</div>
+        <div class="step3">3</div>
+    </div>
+    <div class="text-step uno">
+        <h1>Paso 1</h1>
+        <p>Selecciona el plan que deseas.</p>
+    </div>
+    <div class="text-step dos">
+        <div class="dos-1">
+            <h1>Paso 2</h1>
+            <p>¡Estás a un paso de adquirir tu seguro! Completa los datos para continuar.</p>
+        </div>
+        <div class="dos-2">
+            <h1>Validación</h1>
+            <p>Te hemos enviado un mensaje de texto SMS con el codigo de verificación a tu número de celular:</p>
+            <div class="phone-conf">+57 3227409090</div>
+        </div>
+        <div class="text-step uno">
+            <h1>Paso 3</h1>
+            <p>Resumen de la compra.</p>
+        </div>       
+    </div>
+    <div class="paso1 order-item">
+        <div class="radio-group">
+            <div class="info-name row-fw">
+                <div class="card radio" data-value="Plan A">
+                    <div class="item-sup-v">
+                        <h3 class="tittle-card">PLAN A</h3>
+                        <div class="totals">$ 50.000 <span class="small">/año</span></div>
+                    </div>
+                    <div class="plans">
+                        <div class="line-div">
+                            <div class="icon-card">Renta diaria por incapacidad accidental</div>
+                            <div class="price">$ 20.000</div>
+                        </div>
+                        <div class="line-div">
+                            <div class="icon-card">Muerte accidental</div>
+                            <div class="price">$ 5’000.000</div>
+                        </div>
+                        <div class="line-div">
+                            <div class="icon-card">Incapacidad total y permanente accidental</div>
+                            <div class="price">$ 5’000.000</div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="card radio" data-value="Plan B">
+                    <div class="item-sup-v">
+                        <h3 class="tittle-card">PLAN B</h3>
+                        <div class="totals">$ 97.500 <span class="small">/año</span></div>
+                    </div>
+                    <div class="plans">
+                        <div class="line-div">
+                            <div class="icon-card">Renta diaria por incapacidad accidental</div>
+                            <div class="price">$ 40.000</div>
+                        </div>
+                        <div class="line-div">
+                            <div class="icon-card">Muerte accidental</div>
+                            <div class="price">$ 10’000.000</div>
+                        </div>
+                        <div class="line-div">
+                            <div class="icon-card">Incapacidad total y permanente accidental</div>
+                            <div class="price">$ 10’000.000</div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <input type="text" class="itemval" id="" name="radio-value" readonly>
+        </div>
+    </div>
+    <div class="paso2">
+        <div class="info-name">
+            <div class="material-input">
+                <input type="number" name="" id="" placeholder="&nbsp;">
+                <label>Cédula::</label>
+            </div>
+        </div>
+        <div class="date-input material-input">
+            <input type="date" name="" id="">
+            <label>Fecha de expedición de la cédula:</label>
+        </div>  
+        <div class="info-name">
+            <div class="material-input">
+                <input type="text" name="" id="" placeholder="&nbsp;">
+                <label>Ocupación:</label>
+            </div>
+        </div>
+    </div>
+    <div class="paso2-verify">
+        <div class="info-name">
+           <!-- CAMBIAR POR CODE VERIFY -->
+            <input type="number" name="" id="">
+            <div class="resend">¿Reenviar código?</div>
+        </div>
+        <div class="row-submit"><div id="verify" class="next">Verificar</div></div>
+    </div>
+    <div class="paso3">
+        <div class="resume">
+            <div class="item-tittle">Plan A</div>
+            <div class="data-name">Nombres: <span>Michael Martinez</span></div>
+            <div class="data-id">Cédula: <span>1022943269</span></div>
+            <div class="data-mail">Correo Electrónico: <span>michael@gmail.com</span></div>
+            <div class="data-phone">Celular: <span>3227409090</span></div>
+            <div class="data-total">Total a pagar: <span>50.000</span></div>
+        </div>
+        <img src="http://tudiarioasegurado.artico.website/wp-content/uploads/2021/07/pay.png" alt="formas de pago">
+        <div class="info-name">
+            <div class="material-inline">
+                <label class="contain-check">
+                    <p class="aceppt">Acepto los términos, condiciones, declaraciones y autorizaciones de la poliza.
+                    </p>
+                    <input type="checkbox" name="" id="" data-required="1"><span class="checkmark" checked="checked"></span>
+                    <span class="checkmark"></span>
+                </label>
+            </div>
+        </div>
+        <div class="conclusion">Recibirás un correo con toda la información relacionada con tu poliza, tambien allí podrás designar tus beneficiarios. Mientras esto ocurre, los beneficiarios de tu seguro serán los de ley, es decir: tu cónyuge o compañero(a) permanente, en la mitad del seguro, y tus herederos en la otra mitad.
+        </div>
+        <input type="submit" value="Pagar">
+
+    </div>
+
+    <div class="buttons-steps">
+        <div class="row-submit"><div id="next" class="next">Continuar</div></div>
+        <div class="row-submit"><div id="prev" class="prev">Regresar</div></div>
+    </div>
+</form>
